3adbadd69244987b2e3c5a634a1822958a0fdd90
Yklee <yklee523@gail.com>
2023-09-21 21:41:32 +0800

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
-# 讀取檔案
+#檢查檔案是否存在
+import os    #operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:     #移除欄位
-            continue      #跳到下一迴
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+    print('找到檔案')
+    with open('products.csv', 'r', encoding='utf-8') as f:    # 讀取檔案
+        for line in f:
+            if '商品,價格' in line:     #移除欄位
+                continue      #跳到下一迴
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案')
 
 # 讓使用者輸入
 products = []
